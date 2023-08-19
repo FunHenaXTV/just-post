02d5255de940bc39103f525475e8d206f3514af3
pavel76555 <pavli.strukov@gmail.com>
2023-08-19 21:50:46 +0300

    Add checking validness of email and password

--- src/create_user.cpp
+++ src/create_user.cpp
@@ -1,6 +1,7 @@
 #include "create_user.hpp"
 
 #include <fmt/format.h>
+#include <regex>
 
 #include <userver/clients/dns/component.hpp>
 #include <userver/components/component.hpp>
@@ -11,6 +12,7 @@
 #include <userver/utils/assert.hpp>
 
 namespace just_post {
+  static constexpr int MIN_SIZE_OF_PSWD = 8;
 
 namespace {
 
@@ -31,10 +33,16 @@ class CreateUser final : public userver::server::handlers::HttpHandlerBase {
       userver::server::request::RequestContext&) const override {
     const auto& email = request.GetArg("email");
     const auto& passwd = request.GetArg("passwd");
-    if (email.empty() || passwd.empty()) {
+    if (!IsValidEmail(email)) {
       throw userver::server::handlers::ClientError(
           userver::server::handlers::ExternalBody{
-              "Incorrect Parametrs\n"});
+              "Email is invalid\n"});
+    }
+
+    if (!IsValidPasswd(passwd)) {
+      throw userver::server::handlers::ClientError(
+          userver::server::handlers::ExternalBody{
+              "Password must contain at least 8 symbols\n"});
     }
 
     auto hash_passwd = userver::crypto::hash::Sha512(passwd);
@@ -75,6 +83,19 @@ class CreateUser final : public userver::server::handlers::HttpHandlerBase {
 
 void AppendCreateUser(userver::components::ComponentList& component_list) {
   component_list.Append<CreateUser>();
+}
+
+bool IsValidEmail(const std::string &s) {
+  if (s.empty()) {
+    return false;
+  }
+
+  const std::regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+  return std::regex_match(s, pattern);
+}
+
+bool IsValidPasswd(const std::string &s) {
+  return s.size() >= MIN_SIZE_OF_PSWD;
 }
 
 }  // namespace just_post
