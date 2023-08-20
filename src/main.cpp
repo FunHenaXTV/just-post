d67d2c645925f35a1a329e3328a6b7b20cf2f044
nastya_pion <nastyapion1990@gmail.com>
2023-08-20 03:33:48 +0300

    Add create_post

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "echo.hpp"
 #include "hello.hpp"
 #include "add-user_data.hpp"
+#include "create_post.hpp"
 
 int main(int argc, char* argv[]) {
   auto component_list =
@@ -26,6 +27,7 @@ int main(int argc, char* argv[]) {
   just_post::AppendHello(component_list);
   just_post::AppendEcho(component_list);
   just_post::AppendAddUserData(component_list);
+  just_post::AppendCreatePost(component_list);
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
